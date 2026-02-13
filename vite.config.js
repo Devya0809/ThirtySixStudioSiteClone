99d464ccd44b6a5bd8868920d626d565afe2579f
Devya0809 <157811324+Devya0809@users.noreply.github.com>
2026-02-13 22:14:32 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,29 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    minify: 'terser',
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react') || id.includes('react-dom')) {
+              return 'vendor';
+            }
+            if (id.includes('gsap') || id.includes('@gsap')) {
+              return 'gsap';
+            }
+            if (id.includes('locomotive-scroll')) {
+              return 'locomotive';
+            }
+          }
+        }
+      }
+    }
+  },
+  optimizeDeps: {
+    include: ['react', 'react-dom', 'gsap', '@gsap/react', 'locomotive-scroll']
+  }
 })
